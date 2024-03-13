1dc0ab79c5704abd149c853e40ea811f896394c8
confidential-nt <youyoy689@gmail.com>
2024-03-14 01:44:24 +0900

    Title: 모이자, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240314/모이자/gather.py
+++ 240314/모이자/gather.py
@@ -1,11 +1,11 @@
 def solution():
     n = int(input())
     people = list(map(int, input().split()))
-    result = float("inf")
+    result = float("inf") # 그냥 최소값을 구하면 되는것이므로 배열에 저장할 필요 x.
     for i in range(n):
-        sum = 0
+        sum = 0 
         for j in range(n):
-            sum += people[j] * abs((j - i))
+            sum += people[j] * abs((j - i)) # j == i 면 어차피 0 이니까 분기처리 필요 x.
         result = min(result, sum)    
     print(result)
 
