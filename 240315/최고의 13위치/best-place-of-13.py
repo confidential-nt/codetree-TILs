628cc6f78f0c7d178617a8ad1c091f84c40c2771
confidential-nt <youyoy689@gmail.com>
2024-03-15 19:11:25 +0900

    Title: 최고의 13위치, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240315/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,20 @@
+def solution():
+    N = int(input())
+    matrix = []
+    for _ in range(N):
+        matrix.append(list(map(int, input().split())))
+
+    max_count = 0
+    for i in range(N):
+        for j in range(N):
+            di = i
+            dj = j + 2
+            if 0 <= di < N and 0 <= dj < N:
+                result = matrix[di][i:dj+1].count(1)
+                max_count = max(result, max_count)
+    print(max_count)
+
+
+
+
+solution()
