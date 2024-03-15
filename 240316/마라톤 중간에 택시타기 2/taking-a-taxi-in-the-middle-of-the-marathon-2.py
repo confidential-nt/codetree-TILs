ad818e14e9717c87b6fa05e34ea7aad777414646
confidential-nt <youyoy689@gmail.com>
2024-03-16 03:13:22 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240316/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240316/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,8 +1,10 @@
+import sys
+
 def solution():
     N = int(input())
     arr = [tuple(map(int, input().split())) for _ in range(N)]
     
-    min_distance = float("inf")
+    min_distance = sys.maxsize
     for i in range(1, N - 1):
         temp = arr[i]
         arr[i] = (0,0)
@@ -14,7 +16,6 @@ def solution():
 def get_distance(arr, N):
     result = 0
     for i in range(2, N + 1):
-        # for j in range(i-2, i):
         result += (abs(arr[i - 2][0] - arr[i - 1][0]) + abs(arr[i - 2][1] - arr[i - 1][1]))
     return result
 
