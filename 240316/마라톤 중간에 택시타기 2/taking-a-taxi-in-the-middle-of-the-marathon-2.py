4b2f8e789b24770dd24064b62b1d91537c6047da
confidential-nt <youyoy689@gmail.com>
2024-03-16 03:37:38 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 249ms, Memory: 25MB, Status: Passed - CodeTree

--- 240316/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240316/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,19 +1,20 @@
 def solution():
-    N = int(input())
-    arr = [tuple(map(int, input().split())) for _ in range(N)]
-    
+    n = int(input())
+    arr = [
+        list(map(int, input().split()))
+        for _ in range(n)
+    ]
+
     min_distance = float("inf")
-    for i in range(1, N - 1):
-        distance = get_distance(arr[:i] + arr[i+1:], N - 1)
-        min_distance = min(distance, min_distance)
-        
+    for i in range(1, n - 1):
+        dist = 0
+        prev_index = 0
+        # 굳이 또 배열을 생성하지 않고도 더할 수 있다.
+        for j in range(1, n):
+            if j == i :
+                continue
+            dist += abs(arr[prev_index][0] - arr[j][0]) + abs(arr[prev_index][1] - arr[j][1])
+            prev_index = j
+        min_distance = min(min_distance, dist)
     print(min_distance)
-
-def get_distance(arr, N):
-    result = 0
-    for i in range(2, N + 1):
-        result += (abs(arr[i - 2][0] - arr[i - 1][0]) + abs(arr[i - 2][1] - arr[i - 1][1]))
-    return result
-
-
 solution()
