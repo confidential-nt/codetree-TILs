7a8db37e1efb3152f32f12464fcc892fa21d3b2e
confidential-nt <youyoy689@gmail.com>
2024-03-16 02:14:06 +0900

    Title: 이상한 진수 2, Time: 169ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/이상한 진수 2/awkward-digits-2.py
+++ 240316/이상한 진수 2/awkward-digits-2.py
@@ -1,28 +1,18 @@
 def solution():
-    a = [int(el) for el in input()]
-    n = len(a)
+    a = list(map(int, list(input())))
+    length = len(a)
+
     max_number = 0
-    for i in range(n):
-        if a[i] == 0:
-            a[i] = 1
-            max_number = max(max_number, get_number(a,n))
-            a[i] = 0
-        else:
-            a[i] = 0
-            max_number = max(max_number, get_number(a,n))
-            a[i] = 1
+    for i in range(length):
+        a[i] = 1 - a[i]
+
+        num = 0
+        for j in range(length):
+            num = num * 2 + a[j]
+        max_number = max(num, max_number)
+
+        a[i] = 1 - a[i]
+
     print(max_number)
-
-
-def get_number(arr, n):
-    result = 0
-    for i in range(n):
-        result += arr[i] * (2 ** (n - i- 1))
-    return result
-
-
-
-
-
 
 solution()
