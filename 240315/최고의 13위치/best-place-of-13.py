e917430db746e291970446830ac6920449279012
confidential-nt <youyoy689@gmail.com>
2024-03-15 19:23:31 +0900

    Title: 최고의 13위치, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/최고의 13위치/best-place-of-13.py
+++ 240315/최고의 13위치/best-place-of-13.py
@@ -1,17 +1,12 @@
 def solution():
     N = int(input())
-    matrix = []
-    for _ in range(N):
-        matrix.append(list(map(int, input().split())))
+    matrix = [list(map(int, input().split())) for _ in range(N)]
 
     max_count = 0
     for i in range(N):
-        for j in range(N):
-            di = i
-            dj = j + 2
-            if 0 <= di < N and 0 <= dj < N:
-                result = matrix[di][j:dj+1].count(1)
-                max_count = max(result, max_count)
+        for j in range(N - 2):
+            count = matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2]
+            max_count = max(count, max_count)
     print(max_count)
 
 
