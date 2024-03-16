2bfd610ac721c56c24eae611323b6ccf164f3570
confidential-nt <youyoy689@gmail.com>
2024-03-17 05:46:51 +0900

    Title: 원 모양으로 되어있는 방, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240317/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240317/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -1,19 +1,15 @@
 def solution():
     N = int(input())
     arr = [int(input()) for _ in range(N)]
-    min_distance = float("inf")
-    for i in range(N): # i번째에서 시작한다.
-        dist = 0 # 거리를 계산한다
+
+    min_dist = float("inf")
+    for i in range(N):
+        sum_dist = 0
         for j in range(N):
-            if j - i > 0:
-                dist += arr[j] * (j - i)
-                # print(i, "->", j, arr[j] * (j - i))
-            elif j - i < 0:
-                dist += arr[j] * (N - i + j)    
-                # print(i, "->", j, arr[j] * (N - 1 - i + j))
-        min_distance = min(min_distance, dist)
-        # print(i, min_distance)    
-    print(min_distance) 
-    
+            dist = (j + N - i) % N # 이렇게하면 부호 판단 안해도 됨 
+            sum_dist += (dist) * arr[j]
+        min_dist = min(min_dist, sum_dist)
+    print(min_dist)
+
 
 solution()
