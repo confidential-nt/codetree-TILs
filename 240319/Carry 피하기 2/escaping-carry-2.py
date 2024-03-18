00df4b4d2e9e076018bdbf7861d4445917634227
confidential-nt <youyoy689@gmail.com>
2024-03-19 01:41:32 +0900

    Title: Carry 피하기 2, Time: 162ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240319/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,30 @@
+def solution():
+    n = int(input())
+    nums = [int(input()) for _ in range(n)]
+    answer = 0
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if not is_carry(nums[i],nums[j],nums[k]):
+                    answer = max(answer, nums[i] + nums[j] + nums[k])
+    print(answer)
+
+def is_carry(a,b,c):
+    l_a = [int(el) for el in str(a)][::-1]
+    l_b = [int(el) for el in str(b)][::-1]
+    l_c = [int(el) for el in str(c)][::-1]
+    
+    max_len = max(len(l_a), len(l_b), len(l_c))
+
+    for i in range(max_len):
+        a_el = l_a[i] if len(l_a) >= i + 1 else 0
+        b_el = l_b[i] if len(l_b) >= i + 1 else 0
+        c_el = l_c[i] if len(l_c) >= i + 1 else 0
+        
+        sums = a_el + b_el + c_el
+        if sums >= 10:
+            return True
+    return False
+
+
+solution()
