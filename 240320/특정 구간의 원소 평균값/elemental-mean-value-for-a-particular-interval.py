ee09e7b251c403a05073ed290ab85fb88b4bdf42
confidential-nt <youyoy689@gmail.com>
2024-03-20 00:46:15 +0900

    Title: 특정 구간의 원소 평균값, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -4,18 +4,9 @@ def solution():
     count = 0
     for i in range(N):
         for j in range(i, N):
-            sum_val = 0
-            el_count = 0
-            for k in range(i,j + 1):
-                sum_val += arr[k]
-                el_count += 1
-            avg = sum_val / el_count
-            exists = False
-            for k in range(i,j+1):
-                if avg == arr[k]:
-                    exists = True
-            if exists:
-                count += 1        
+            avg = sum(arr[i:j+1]) / len(arr[i:j+1])
+            if avg in arr[i:j+1]:
+                count += 1
     print(count)
 
 
