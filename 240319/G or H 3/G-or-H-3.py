c0d313b0a1ee4b8b592822d9a2e83b0cfae342cb
confidential-nt <youyoy689@gmail.com>
2024-03-19 23:01:45 +0900

    Title: G or H 3, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240319/G or H 3/G-or-H-3.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+def solution():
+    N,K = map(int, input().split())
+    input_dict = defaultdict(int)
+    for _ in range(N):
+        loc, alpha = input().split()
+        loc = int(loc)
+        input_dict[loc] = 2 if alpha == "H" else 1
+    arr = list(input_dict.keys())
+    R = max(arr)
+    placed = [0] * (R + 1)
+
+    for el in arr:
+        placed[el] = input_dict[el]
+    
+    max_score = 0
+    for i in range(1, R - K + 1):
+        score = 0
+        for j in range(i, i+K+1):
+            score += placed[j]
+        max_score = max(max_score, score)
+    print(max_score)
+
+
+solution()
