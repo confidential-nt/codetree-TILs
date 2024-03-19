6ba9e1dc9d17e06cab483bef8a5e374a359f65b5
confidential-nt <youyoy689@gmail.com>
2024-03-20 01:06:58 +0900

    Title: 특정 구간의 원소 평균값, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -3,8 +3,8 @@ def solution():
     arr = list(map(int, input().split()))
     count = 0
     for i in range(N):
-        for j in range(i, N):
-            avg = sum(arr[i:j+1]) / len(arr[i:j+1])
+        for j in range(i, N): # 범위 선택 주의. j를 i + 1 부터 시작하게 하니까 틀림. 이유? 잘 모르겠음..뭔가 오류가 있나봄.
+            avg = sum(arr[i:j+1]) / len(arr[i:j+1]) # 실수 나눗셈을 하지 않으면 자연수가 아닌 경우까지 포함됨.
             if avg in arr[i:j+1]:
                 count += 1
     print(count)
