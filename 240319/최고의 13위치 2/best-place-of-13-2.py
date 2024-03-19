1beba4728e36814e1ab956a632b2f021709cb00c
confidential-nt <youyoy689@gmail.com>
2024-03-19 20:11:35 +0900

    Title: 최고의 13위치 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/최고의 13위치 2/best-place-of-13-2.py
+++ 240319/최고의 13위치 2/best-place-of-13-2.py
@@ -4,10 +4,13 @@ def solution():
     max_count = 0
     for i in range(N):
         for j in range(N - 2):
-            for l in range(i+1, N):
+            for l in range(N):
                 for k in range(N - 2):
+                    if abs(j - k) < 3 :
+                        continue
                     count1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
                     count2 = arr[l][k] + arr[l][k + 1] + arr[l][k + 2]
+                    
                     max_count = max(max_count, count1 + count2)
     print(max_count)
 
