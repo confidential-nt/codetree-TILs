afa748cb3af15df9bd809b16cd0a94fa8e21c794
confidential-nt <youyoy689@gmail.com>
2024-03-19 22:07:58 +0900

    Title: 특정 수와 근접한 합, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240319/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240319/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,15 +1,12 @@
 def solution():
     N,S = map(int, input().split())
     arr = list(map(int, input().split()))
-
+    arr_sum = sum(arr)
     answer = float("inf")
     for i in range(N):
         for j in range(i+1,N):
-            sum_arr = arr[:]
-            sum_arr[i] = 0
-            sum_arr[j] = 0
             
-            sum_result = sum(sum_arr)
+            sum_result = arr_sum - arr[i]- arr[j]
             
             answer = min(answer, abs(S - sum_result))
     print(answer)
