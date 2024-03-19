ea86edcf7749be1319997973fecae2443b50e599
confidential-nt <youyoy689@gmail.com>
2024-03-19 23:13:51 +0900

    Title: G or H 3, Time: 208ms, Memory: 25MB, Status: Passed - CodeTree

--- 240319/G or H 3/G-or-H-3.py
+++ 240319/G or H 3/G-or-H-3.py
@@ -2,17 +2,13 @@ from collections import defaultdict
 
 def solution():
     N,K = map(int, input().split())
-    input_dict = defaultdict(int)
+    R = 10000 # 위치의 범위가 10000으로 고정되어있다는 점에 유의. -> 10000인 위치까지 봐야한다.
+    placed = [0] * (R + 1)
+
     for _ in range(N):
         loc, alpha = input().split()
         loc = int(loc)
-        input_dict[loc] = 2 if alpha == "H" else 1
-    arr = list(input_dict.keys())
-    R = 10000 # 위치의 범위가 10000으로 고정되어있다는 점에 유의.
-    placed = [0] * (R + 1)
-
-    for el in arr:
-        placed[el] = input_dict[el]
+        placed[loc] = 2 if alpha == "H" else 1
     
     max_score = 0
     for i in range(1, R - K + 1):
