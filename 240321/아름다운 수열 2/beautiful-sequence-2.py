9f084e6f280d867cbbfd02ac45a7eef962d0a0ee
confidential-nt <youyoy689@gmail.com>
2024-03-21 07:26:43 +0900

    Title: 아름다운 수열 2, Time: 1531ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240321/아름다운 수열 2/beautiful-sequence-2.py
+++ 240321/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,22 +4,16 @@ def solution():
     N,M = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    
+    all_permutations = set(permutations(B,M))
     
     count = 0
     for i in range(N):
-        
         piece = A[i:i+M]
         if len(piece) != M:
             break
-        
-        is_beautiful = True
-        for el in piece:
-            if el not in B:
-                is_beautiful = False
-                break
-        if is_beautiful:
-            count +=1
+        for perm in all_permutations:
+            if tuple(piece) == perm:
+                count += 1
     print(count)
 
 
