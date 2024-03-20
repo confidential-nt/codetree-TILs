e6c661327dbcc71e3cdfbbbf4d0fefeddab58829
confidential-nt <youyoy689@gmail.com>
2024-03-21 07:14:57 +0900

    Title: 아름다운 수열 2, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+def solution():
+    N,M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    all_combinations = list(permutations(B, len(B)))
+    
+    count = 0
+    for i in range(N):
+        for j in range(i, N):
+            piece = A[i:j+1]
+            for comb in all_combinations:
+                if comb == tuple(piece):
+                    count += 1
+    print(count)
+
+
+
+solution()
