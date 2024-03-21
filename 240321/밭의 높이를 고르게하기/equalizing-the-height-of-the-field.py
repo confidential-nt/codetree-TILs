6b58aad240b08ebc86f2f93b8e42b1cf29adeb3b
confidential-nt <youyoy689@gmail.com>
2024-03-21 21:54:03 +0900

    Title: 밭의 높이를 고르게하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,14 @@
+def solution():
+    N,H,T = map(int, input().split()) # 연속하게 최소 T번 이상 H 높이
+    field = list(map(int, input().split()))
+
+    min_price = float("inf")
+
+    for i in range(N - T):
+        price = 0
+        for j in range(i, i + T):
+            price += abs(field[j] - H)
+        min_price = min(price, min_price)
+    print(min_price)
+
+solution()
