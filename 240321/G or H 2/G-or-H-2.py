0ef6a6d1f8064d17232e7bb9135c563477ca2f87
confidential-nt <youyoy689@gmail.com>
2024-03-21 21:46:16 +0900

    Title: G or H 2, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/G or H 2/G-or-H-2.py
+++ 240321/G or H 2/G-or-H-2.py
@@ -10,24 +10,13 @@ def solution():
         coord = int(coord)
         placed[coord] = alpha
     max_size = 0    
-    for i in range(100):
+    for i in range(101):
         for j in range(i+1, 101):
             if placed[i] == 0:
                 continue
             if placed[j] == 0:
                 continue    
             arr = placed[i:j+1]
-            is_all_same = True
-            compare = arr[0]
-            for el in arr[1:]:
-                if el != compare:
-                    is_all_same = False
-                    break
-                compare = el
-
-            if is_all_same:
-                max_size = max(max_size, j - i)
-                continue
 
             num_of_g = 0
             num_of_h = 0
@@ -36,7 +25,8 @@ def solution():
                     num_of_g += 1
                 elif el == "H":
                     num_of_h += 1
-            if num_of_g == num_of_h:
+            # 한쪽이 0 이면 다른 한쪽으로만 채워졌다는 이야기 -> is_all_same 구할 필요 x.    
+            if num_of_g == 0 or num_of_h == 0 or num_of_g == num_of_h:
                 max_size = max(max_size, j - i)   
 
 
