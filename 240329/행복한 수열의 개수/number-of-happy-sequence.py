653594c03c19952abce049f165b058a99a2725ca
confidential-nt <youyoy689@gmail.com>
2024-03-29 23:12:27 +0900

    Title: 행복한 수열의 개수, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240329/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240329/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,8 +4,12 @@ def solution():
     # 각각 열,행쪽 좌표가 n보다 크거나 같은지 확인
     # 연속으로 m개 이상 같은 숫자가 등장하는지 확인.
 
+    # 고려 사항
     # 연속으로 m개이상 같은 숫자가 등장하는지 어떻게 확인? 
     # 행의 경우: i고정, 열의 경우 n까지 루프. -> before, current를 통해 숫자 비교 -> 같다면 count += 1 / 다르다면 count 냅두거나 1로 초기화
+    # + 주의 : 끊기는 지점에서 연속된 숫자가 몇 번 나왔는지 count를 한번 살펴봐야 함.
+    # 주의: 연속되는 수의 개수를 구하는 게 아니라, 단지 m개 이상의 연속된 수가 나오는 수열을 구하는 것임.
+
     n,m = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
     answer = 0
@@ -22,7 +26,7 @@ def solution():
                 if current == before:
                     count += 1
                 else:
-                    if count >= m:
+                    if count >= m: # 이미 만족했다면 더 이상 셀 필요가 없음. 이미 그 수열은 답에 포함됨.
                         answer += 1
                         end = True
                         break
