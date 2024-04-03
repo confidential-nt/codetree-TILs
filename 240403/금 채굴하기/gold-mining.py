93b0f8c77eb8cd3fdf26139021cfb0d1b88b312f
confidential-nt <youyoy689@gmail.com>
2024-04-03 23:10:12 +0900

    Title: 금 채굴하기, Time: 537ms, Memory: 27MB, Status: Passed - CodeTree

--- 240403/금 채굴하기/gold-mining.py
+++ 240403/금 채굴하기/gold-mining.py
@@ -1,13 +1,3 @@
-# 흐름
-# 격자 바깥을 빠져나갈 수 있음. -> 중심점 (i,j) 단위로 K를 0부터 늘려나가면서
-# 각 단계에서 금이 몇 개 있는지 파악 -> 금 가격과 K 격자의 비용 비교
-# 손해라면 끝내기 -> 최대 금 개수 저장 (이미 손해라면 K를 증가시켜서 금 하나를 더 얻을 수 있어도 이미 손해이지 않을까 싶은 생각인데..아닌 것 같음. 그냥 다 해봐야할듯.) 
-# 손해가 아니라면 K를 증가시킴
-
-# 고려사항
-# K를 어떻게 구현할 것이냐? 0일때는 특별 경우로 취급.((i,j)에 있는 값만 파악) 
-# 1일 때부터 상하좌우 dx,dy?
-
 def solution():
     n,m = map(int, input().split())
     grid = [list(map(int, input().split())) for _ in range(n)]
@@ -18,6 +8,8 @@ def solution():
             for i in range(n)
             for j in range(n)
             if abs(row - i) + abs(col - j) <= k
+            # 굳이 마름모를 직접 그리는 것을 가정하지 않아도 간접적으로 도출할 수 있음.
+            # 어떻게? 모든 격자의 각 칸에 대하여, 이처럼 거리를 계산하여 k라는 거리 안에 들어오는가를 계산해서.
         ])
 
     def get_area(k):
@@ -26,7 +18,8 @@ def solution():
     max_gold = 0
     for i in range(n):
         for j in range(n):
-            for k in range(2 * (n - 1) + 1):
+            for k in range(2 * (n - 1) + 1): # 나는 손해가 나는 즉시 계산을 중단해도 괜찮다고 생각했는데 더 안전빵으로 그냥 nxn격자를 다 채울 범위까지 반복하는 게 나은듯.
+            # 다만 k를 어디까지 늘릴거냐는 정하기 나름인듯. 물론 어쨌든 격자를 다 채워야하긴 하는 것 같다만..
                 num_of_gold = get_num_of_gold(i,j,k)
 
                 if num_of_gold * m >= get_area(k):
