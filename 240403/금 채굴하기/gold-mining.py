00afa1b19b7ffaedcc78705a01827c23b01dc0dc
confidential-nt <youyoy689@gmail.com>
2024-04-03 23:01:19 +0900

    Title: 금 채굴하기, Time: 499ms, Memory: 29MB, Status: Passed - CodeTree

--- 240403/금 채굴하기/gold-mining.py
+++ 240403/금 채굴하기/gold-mining.py
@@ -1,36 +1,36 @@
-# 흐름
-# 격자 바깥을 빠져나갈 수 있음. -> 중심점 (i,j) 단위로 K를 0부터 늘려나가면서
-# 각 단계에서 금이 몇 개 있는지 파악 -> 금 가격과 K 격자의 비용 비교
-# 손해라면 끝내기 -> 최대 금 개수 저장 (이미 손해라면 K를 증가시켜서 금 하나를 더 얻을 수 있어도 이미 손해이지 않을까 싶은 생각인데..아닌 것 같음. 그냥 다 해봐야할듯.) 
-# 손해가 아니라면 K를 증가시킴
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-# 고려사항
-# K를 어떻게 구현할 것이냐? 0일때는 특별 경우로 취급.((i,j)에 있는 값만 파악) 
-# 1일 때부터 상하좌우 dx,dy?
 
-def solution():
-    n,m = map(int, input().split())
-    grid = [list(map(int, input().split())) for _ in range(n)]
+# 주어진 k에 대하여 마름모의 넓이를 반환합니다.
+def get_area(k):
+    return k * k + (k + 1) * (k + 1)
 
-    def get_num_of_gold(row,col,k):
-        return sum([
-            grid[i][j]
-            for i in range(n)
-            for j in range(n)
-            if abs(row - i) + abs(col - j) <= k
-        ])
 
-    def get_area(k):
-        return k * k + (k + 1) * (k + 1)
+# 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
+def get_num_of_gold(row, col, k):
+    return sum([
+        grid[i][j]
+        for i in range(n)
+        for j in range(n)
+        if abs(row - i) + abs(col - j) <= k
+    ])
 
-    max_gold = 0
-    for i in range(n):
-        for j in range(m):
-            for k in range(2 * (n - 1) + 1):
-                num_of_gold = get_num_of_gold(i,j,k)
 
-                if num_of_gold * m >= get_area(k):
-                    max_gold = max(max_gold, num_of_gold)
-    print(max_gold)
+max_gold = 0
 
-solution()
+# 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구합니다.
+for row in range(n):
+    for col in range(n):
+        for k in range(2 * (n - 1) + 1):
+            num_of_gold = get_num_of_gold(row, col, k)
+            
+            # 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수를 저장합니다.
+            if num_of_gold * m >= get_area(k):
+                max_gold = max(max_gold, num_of_gold)
+
+print(max_gold)
