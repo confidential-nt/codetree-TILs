f67b9d1855e681f264a51646eb2b9c359b5d5149
confidential-nt <youyoy689@gmail.com>
2024-04-15 16:50:56 +0900

    Title: 삼각형 만들기, Time: 183ms, Memory: 26MB, Status: Passed - CodeTree

--- 240415/삼각형 만들기/create-triangle.py
+++ 240415/삼각형 만들기/create-triangle.py
@@ -17,15 +17,10 @@ def is_triangle(c1,c2,c3):
     x2,y2 = c2
     x3,y3 = c3
 
-    # (x3 - x2) * (y2 - y1)
-
-    if y1 - y2 == 0 and x2 - x3 == 0:
+    if (x1 == x2 and y2 == y3) or (x1 == x3 and y3 == y2) or (x2 == x3 and y3 == y1) or (x2 == x1 and y1 == y3) or (x3 == x1 and y1 == y2) or (x3 == x2 and y2 == y1):
         return True
 
-    if x1 - x3 == 0 and y3 - y2 == 0:
-        return True  
-
-    return False
+    return False    
 
 def get_area(c1,c2,c3):
     x1,y1 = c1
