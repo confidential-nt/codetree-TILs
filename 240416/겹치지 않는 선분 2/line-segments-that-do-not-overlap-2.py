0ebbf9ced7f6c8400a6fbc877169b0caf0cfd1e2
confidential-nt <youyoy689@gmail.com>
2024-04-16 16:18:23 +0900

    Title: 겹치지 않는 선분 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240416/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -10,21 +10,11 @@ def solution():
             x1,x2 = lines[i] # base
             x3,x4 = lines[j] # 비교대상
             
-            # 이렇게 하나 하나 조건 탐색할 경우, 꼼꼼함이 필수. 절망하지 말고 침착하게.
-            if x1 < x2 and x3 < x4:
-                if (x3< x1 <x4 and x3 < x2 < x4) or (x1 < x3 < x2 and x1 < x4 < x2):
-                    count += 1
-            elif x1 > x2 and x3 > x4:
-                if (x4< x1 <x3 and x4 < x2 < x3) or (x2 < x3 < x1 and x2 < x4 < x1):
-                    count += 1
-            elif x1 < x2 and x3 > x4:
-                if (x4 < x2):
-                    count += 1
-            elif x1 > x2 and x3 < x4:
-                if (x2 < x4):
-                    count += 1  
+            if (x1 > x3 and x2 < x4) or (x1 < x3 and x2 > x4):
+                count += 1
+            
     result = n - count
-    print(result if result > 0 else 0)    
+    print(result if result > 0 else 0) # 겹치는 횟수가 아니라 겹치는 선분의 수.    
 
 
 solution()
