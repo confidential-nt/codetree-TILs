f25064c6b3fad055ee80d2d9308ff138d9aae945
confidential-nt <youyoy689@gmail.com>
2024-04-16 15:53:53 +0900

    Title: 겹치지 않는 선분 2, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240416/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -9,9 +9,10 @@ def solution():
                 continue
             x1,x2 = lines[i] # base
             x3,x4 = lines[j] # 비교대상
-            if x3< x1 <x4 and x3 < x2 < x4:
-                count += 2
-    print(n - count)    
+            if (x3< x1 <x4 and x3 < x2 < x4) or (x1 < x3 < x2 and x1 < x4 < x2) or (x4< x1 <x3 and x4 < x2 < x3) or (x2 < x3 < x1 and x2 < x4 < x1):
+                count += 1
+    result = n - count
+    print(result if result > 0 else 0)    
 
 
 solution()
