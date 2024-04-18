1abda03afe112cf3cd02dec8c3cfa8b062d751f5
confidential-nt <youyoy689@gmail.com>
2024-04-18 19:13:32 +0900

    Title: 스승의 은혜 2, Time: 256ms, Memory: 25MB, Status: Passed - CodeTree

--- 240418/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240418/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,23 +1,20 @@
 def solution():
     N,B = map(int, input().split())
-    P = []
-    for _ in range(N):
-        P.append(int(input()))
-    
+    P = [int(input()) for _ in range(N)]
     max_count = 0
     for i in range(N):
+        tmp = [P[j] for j in range(N)]
+        tmp[i] /= 2
+
+        # 정렬;; 그리디 문제 아니여 이거;;
+        tmp.sort()
         count = 0
         total = 0
-        for j in range(N):
-            if i == j:
-                if total + (P[j] // 2) <= B:
-                    total += (P[j] // 2)
-                    count += 1
-                continue
-            if total + (P[j]) <= B:
-                    total += (P[j])
-                    count += 1
-        max_count = max(max_count, count)    
-    print(max_count)
 
+        for j in range(N):
+            if total + tmp[j] <= B:
+                total += tmp[j]
+                count += 1
+        max_count = max(count, max_count)
+    print(max_count)
 solution()
