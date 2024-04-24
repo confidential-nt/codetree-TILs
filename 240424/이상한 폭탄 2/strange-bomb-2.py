4c356419343cc5e710f83734195e894ad772f6ae
confidential-nt <youyoy689@gmail.com>
2024-04-24 21:29:57 +0900

    Title: 이상한 폭탄 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,17 @@
+def solution():
+    N,K = map(int, input().split())
+    seq = [int(input()) for _ in range(N)]
+    max_bomb_number = -1
+
+    for i in range(N):
+        bomb_number = 0
+        for j in range(i+1, N):
+            if seq[i] == seq[j] and j - i <= K:
+                bomb_number = seq[i]
+        max_bomb_number = max(bomb_number, max_bomb_number)        
+
+
+
+    print(max_bomb_number)
+
+solution()
