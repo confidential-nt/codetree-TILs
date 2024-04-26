3d402ef475439cf1ba3f1ffb7440c0ef4c9cd7f8
confidential-nt <youyoy689@gmail.com>
2024-04-26 21:20:13 +0900

    Title: 숫자들의 합 중 최대, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240426/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -3,8 +3,12 @@ def solution():
 
     max_sum = 0
     for num in range(X,Y+1):
-        nums = [int(el) for el in str(num)]
-        sum_res = sum(nums)
-        max_sum = max(max_sum, sum_res)
+        max_sum = max(max_sum, digit_sum(num))
     print(max_sum)
+
+def digit_sum(num):
+    if num < 10:
+        return num
+    else:
+        return digit_sum(num // 10) + num % 10 # 끝자리 제외한 나머지 부분 도려내기 + 끝부분만 도려내기
 solution()
