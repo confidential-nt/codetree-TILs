80544a58756362531db8b561e1940d997b5320d8
confidential-nt <youyoy689@gmail.com>
2024-04-26 20:56:39 +0900

    Title: 스승의 은혜 3, Time: 397ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240426/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,23 @@
+def solution():
+    N,B = map(int, input().split())
+    arr = [tuple(map(int, input().split())) for _ in range(N)] # (선물, 배송비)
+    max_count = 0
+    for i in range(N):
+        tmp = [el for el in arr]
+        tmp[i] = (tmp[i][0]/2, tmp[i][1])
+        
+        tmp.sort(key=lambda x:x[0])
+
+        count = 0
+        total = 0
+
+        for j in range(N):
+            if total + tmp[j][0] + tmp[j][1] <= B:
+                total += tmp[j][0] + tmp[j][1]
+                count += 1
+        max_count = max(count, max_count)
+
+    print(max_count)    
+
+
+solution()
