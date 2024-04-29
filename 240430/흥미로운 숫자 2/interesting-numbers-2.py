4d10904bf73312f9ce2187182b900451ab94b589
confidential-nt <youyoy689@gmail.com>
2024-04-30 00:36:32 +0900

    Title: 흥미로운 숫자 2, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240430/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,13 +3,22 @@ from collections import defaultdict
 def solution():
     X,Y = map(int, input().split())
     answer = 0
-    for num in range(X, Y + 1):
-        nums_sep = [int(el) for el in str(num)]
-        counter = defaultdict(int)
-        for sep in nums_sep:
-            counter[sep] += 1
+    for i in range(X, Y + 1):
+        num = i
+        digit = [0] * 10 # 0 ~ 9
+        all_digits = 0
+        while num:
+            digit[num % 10] += 1
+            all_digits += 1
+            num //= 10
         
-        if len(counter.keys()) == 2 and 1 in counter.values():
+        interesting = False
+
+        for j in range(10):
+            if digit[j] == all_digits - 1:
+                interesting = True
+
+        if interesting:
             answer += 1
     print(answer)    
 solution()
