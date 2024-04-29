2b54e6ebec47ecfdb00e7672a9e3f4902d87bef8
confidential-nt <youyoy689@gmail.com>
2024-04-30 00:37:29 +0900

    Title: 흥미로운 숫자 2, Time: 180ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240430/흥미로운 숫자 2/interesting-numbers-2.py
@@ -7,7 +7,7 @@ def solution():
         num = i
         digit = [0] * 10 # 0 ~ 9
         all_digits = 0
-        while num:
+        while num: # 저번에도 그렇고 자릿수 관련한 계산은 이렇게 해보자.
             digit[num % 10] += 1
             all_digits += 1
             num //= 10
@@ -15,7 +15,7 @@ def solution():
         interesting = False
 
         for j in range(10):
-            if digit[j] == all_digits - 1:
+            if digit[j] == all_digits - 1: # digit[j]가 all_digits - 1이면 나머지 digit[j]는 1일수 밖에 없음.
                 interesting = True
 
         if interesting:
