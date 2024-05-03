e07f8c3c791f824298adc5f1498ec5796d31854f
Somi Park <youyoy689@gmail.com>
2024-05-04 07:45:52 +0900

    Title: 빙산의 일각 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240504/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240504/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -6,10 +6,13 @@ def solution():
 
     for h in range(1, 1001):
         count = 0
-
+        # 가장 왼쪽에 빙산이 있는 경우의 예외를 처리
         if arr[0] > h :
             count += 1
 
+        # 바로 앞 블록은 해수면에 잠겨있고
+        # 자기 자신의 블록은 해수면 위에 떠있는 경우,
+        # 자기 자신 블록부터 시작하는 빙산이 하나 더 있습니다. -> 이 사실을 못깨달았네..
         for i in range(1, n):
             if arr[i - 1] <= h and arr[i] > h:
                 count += 1
