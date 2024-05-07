6bfcc0cb51f0e9b563c426fe9c1253710e688580
Somi Park <youyoy689@gmail.com>
2024-05-07 18:36:00 +0900

    Title: 3개의 선 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/3개의 선 2/three-lines-2.py
@@ -0,0 +1,34 @@
+from itertools import combinations
+
+def solution():
+    n = int(input())
+    arr = [tuple(map(int, input().split())) for _ in range(n)]
+    answer = 0
+
+    # 가로 x 세로의 조합 (3가지 선택)
+    # 각 조합에서 0~10까지 탐색.
+    for i in range(4): # 가로(y) 0 ~ 3개
+        j = 3 - i # 세로(x) 3 ~ 0 개
+
+        possible = list(range(11))
+        y = list(combinations(possible, i))
+        x = list(combinations(possible, j))
+
+        temp = [0]* n
+        for index, (ax,ay) in enumerate(arr):
+            for cx in x:
+                for cy in y:
+                    if ax in cx:
+                        temp[index] = 1
+                    if ay in cy:
+                        temp[index] = 1   
+
+        if len([el for el in temp if el == 1]) == n:
+            answer = 1
+            break
+
+
+    print(answer)
+
+
+solution()
