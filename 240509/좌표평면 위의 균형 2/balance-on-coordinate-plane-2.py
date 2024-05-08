4251c41a59d3531c1bb7fc5a312e686b6f08b381
Somi Park <youyoy689@gmail.com>
2024-05-09 01:08:28 +0900

    Title: 좌표평면 위의 균형 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240509/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,26 @@
+def solution():
+    n = int(input())
+    points = [tuple(map(int, input().split())) for _ in range(n)]
+
+    min_max_num = float("inf")
+
+    for i in range(1, 101): # x축
+        for j in range(1, 101): # y축
+            if not (i % 2 == 0 and j % 2 == 0):
+                continue
+            nums = [0,0,0,0] # (x작,y크) (x작,y작) (x크,y작) (x크,y크)   
+            for x,y in points:
+                if x < i and y > j:
+                    nums[0] += 1
+                elif x < i and y < j:
+                    nums[1] += 1
+                elif x > i and y < j:
+                    nums[2] += 1
+                elif x > i and y > j:
+                    nums[3] += 1
+            max_num = max(nums)
+            min_max_num = min(min_max_num, max_num)
+
+
+    print(min_max_num)
+solution()
