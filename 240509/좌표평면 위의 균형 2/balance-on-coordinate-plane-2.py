2a438e3e7e219ed7458b15f942f1f3c1a67712d1
Somi Park <youyoy689@gmail.com>
2024-05-09 01:09:26 +0900

    Title: 좌표평면 위의 균형 2, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240509/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 240509/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -7,6 +7,7 @@ def solution():
     for i in range(1, 101): # x축
         for j in range(1, 101): # y축
             if not (i % 2 == 0 and j % 2 == 0):
+                # range(0, 101, 2) 이렇게 해버릴 수도 있음.
                 continue
             nums = [0,0,0,0] # (x작,y크) (x작,y작) (x크,y작) (x크,y크)   
             for x,y in points:
