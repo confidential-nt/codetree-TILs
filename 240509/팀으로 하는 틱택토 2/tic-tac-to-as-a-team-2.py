8fdf14b8afc36916e8e7ff418d75388e44a33419
Somi Park <youyoy689@gmail.com>
2024-05-09 18:38:31 +0900

    Title: 팀으로 하는 틱택토 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,6 +1,7 @@
 def solution():
     tic_tac_toe = [[int(el) for el in input()] for _ in range(3)]
     count = 0
+    # 가로
     for i in range(3):
         a = tic_tac_toe[i][0]
         b = tic_tac_toe[i][1]
@@ -14,15 +15,11 @@ def solution():
         elif b == c and b != a:
             # print(a,b,c)
             count += 1    
-
+    # 세로
     for i in range(3):
         for j in range(3):
-            if j - i < 0:
-                continue
-            if j - i > 1:
-                continue
             for k in range(3):
-                if (i == j == k) or (i != j and k > j):
+                if (i == j == k):
                     a = tic_tac_toe[0][i]
                     b = tic_tac_toe[1][j]
                     c = tic_tac_toe[2][k]
@@ -37,6 +34,47 @@ def solution():
                     elif b == c and b != a:
                         # print(a,b,c)
                         count += 1    
+
+    # 대각선 오른쪽
+    for i in range(3):
+        for j in range(i+1, 3):
+            for k in range(j + 1, 3):
+                a = tic_tac_toe[0][i]
+                b = tic_tac_toe[1][j]
+                c = tic_tac_toe[2][k]
+
+                # a,b 같/ a,c같 / b,c같/
+                if a == b and a != c:
+                    # print(a,b,c)
+                    count += 1
+                elif a == c and a != b:
+                    # print(a,b,c)
+                    count += 1
+                elif b == c and b != a:
+                    # print(a,b,c)
+                    count += 1    
+
+
+    # 대각선 왼쪽
+    for i in range(2, -1, -1):
+        for j in range(i - 1, -1, -1):
+            for k in range(j - 1, -1, -1):
+                a = tic_tac_toe[0][i]
+                b = tic_tac_toe[1][j]
+                c = tic_tac_toe[2][k]
+                # print(a,b,c)
+                # a,b 같/ a,c같 / b,c같/
+                if a == b and a != c:
+                    # print(a,b,c)
+                    count += 1
+                elif a == c and a != b:
+                    # print(a,b,c)
+                    count += 1
+                elif b == c and b != a:
+                    # print(a,b,c)
+                    count += 1   
+
+
     print(count)
     
 solution()
