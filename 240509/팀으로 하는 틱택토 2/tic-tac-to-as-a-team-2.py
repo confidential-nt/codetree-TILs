6581960ff4a27a7db386e9613825ab29b41c1987
Somi Park <youyoy689@gmail.com>
2024-05-09 18:30:38 +0900

    Title: 팀으로 하는 틱택토 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -6,15 +6,20 @@ def solution():
         b = tic_tac_toe[i][1]
         c = tic_tac_toe[i][2]
         if a == b and a != c:
+            # print(a,b,c)
             count += 1
         elif a == c and a != b:
+            # print(a,b,c)
             count += 1
         elif b == c and b != a:
+            # print(a,b,c)
             count += 1    
 
     for i in range(3):
         for j in range(3):
-            if abs(i - j) > 1:
+            if j - i < 0:
+                continue
+            if j - i > 1:
                 continue
             for k in range(3):
                 if (i == j == k) or (i != j and k > j):
@@ -24,10 +29,13 @@ def solution():
 
                     # a,b 같/ a,c같 / b,c같/
                     if a == b and a != c:
+                        # print(a,b,c)
                         count += 1
                     elif a == c and a != b:
+                        # print(a,b,c)
                         count += 1
                     elif b == c and b != a:
+                        # print(a,b,c)
                         count += 1    
     print(count)
     
