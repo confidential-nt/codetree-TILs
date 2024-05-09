af98f129d024beb240e53501ad821fbcda470cc2
Somi Park <youyoy689@gmail.com>
2024-05-09 19:11:21 +0900

    Title: 팀으로 하는 틱택토 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240509/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,110 +1,72 @@
+MAX_A = 3
+MAX_X = 9
+
 def solution():
-    tic_tac_toe = [[int(el) for el in input()] for _ in range(3)]
-    is_already = [[[0] * 10 for _ in range(10)] for _ in range(10)]
-    count = 0
-    # 가로
-    for i in range(3):
-        a = tic_tac_toe[i][0]
-        b = tic_tac_toe[i][1]
-        c = tic_tac_toe[i][2]
 
-        temp = sorted([a,b,c])
-        ta,tb,tc = temp
-        if is_already[ta][tb][tc]:
-            continue
+    board = [list(map(int, input())) for _ in range(MAX_A)]
 
-        if a == b and a != c:
-            # print(a,b,c)
-            count += 1
-        elif a == c and a != b:
-            # print(a,b,c)
-            count += 1
-        elif b == c and b != a:
-            # print(a,b,c)
-            count += 1  
-        sorted_abc = sorted([a,b,c])
-        sa,sb,sc = sorted_abc
-        is_already[sa][sb][sc] = 1
-    # 세로
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-                if (i == j == k):
-                    a = tic_tac_toe[0][i]
-                    b = tic_tac_toe[1][j]
-                    c = tic_tac_toe[2][k]
-                    temp = sorted([a,b,c])
-                    ta,tb,tc = temp
-                    if is_already[ta][tb][tc]:
-                        continue
+    answer = 0
 
-                    # a,b 같/ a,c같 / b,c같/
-                    if a == b and a != c:
-                        # print(a,b,c)
-                        count += 1
-                    elif a == c and a != b:
-                        # print(a,b,c)
-                        count += 1
-                    elif b == c and b != a:
-                        # print(a,b,c)
-                        count += 1
-                    sorted_abc = sorted([a,b,c])
-                    sa,sb,sc = sorted_abc
-                    
-                    is_already[sa][sb][sc] = 1
+    for i in range(1, MAX_X + 1):
+        for j in range(i + 1, MAX_X + 1):
+            # 보드를 직접 탐색하는 것보다, 조합에 대해 먼저 생각. (i,j)는 한팀.
+            win = False
 
-    # 대각선 오른쪽
-    for i in range(3):
-        for j in range(i+1, 3):
-            for k in range(j + 1, 3):
-                a = tic_tac_toe[0][i]
-                b = tic_tac_toe[1][j]
-                c = tic_tac_toe[2][k]
-                temp = sorted([a,b,c])
-                ta,tb,tc = temp
-                if is_already[ta][tb][tc]:
-                    continue
+            num_i = 0
+            num_j = 0
 
-                # a,b 같/ a,c같 / b,c같/
-                if a == b and a != c:
-                    # print(a,b,c)
-                    count += 1
-                elif a == c and a != b:
-                    # print(a,b,c)
-                    count += 1
-                elif b == c and b != a:
-                    # print(a,b,c)
-                    count += 1    
-                sorted_abc = sorted([a,b,c])
-                sa,sb,sc = sorted_abc
-                is_already[sa][sb][sc] = 1
+            # 가로
+            for k in range(MAX_A):
+                num_i = 0
+                num_j = 0
+                for l in range(MAX_A):
+                    if board[k][l] == i: # 인덱싱 주목
+                        num_i += 1
+                    if board[k][l] == j:
+                        num_j += 1
 
-    # 대각선 왼쪽
-    for i in range(2, -1, -1):
-        for j in range(i - 1, -1, -1):
-            for k in range(j - 1, -1, -1):
-                a = tic_tac_toe[0][i]
-                b = tic_tac_toe[1][j]
-                c = tic_tac_toe[2][k]
-                temp = sorted([a,b,c])
-                ta,tb,tc = temp
-                if is_already[ta][tb][tc]:
-                    continue
-                # print(a,b,c)
-                # a,b 같/ a,c같 / b,c같/
-                if a == b and a != c:
-                    # print(a,b,c)
-                    count += 1
-                elif a == c and a != b:
-                    # print(a,b,c)
-                    count += 1
-                elif b == c and b != a:
-                    # print(a,b,c)
-                    count += 1   
-                sorted_abc = sorted([a,b,c])
-                sa,sb,sc = sorted_abc
-                is_already[sa][sb][sc] = 1
+                if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                    win = True
 
-    print(count)
-    
+            
+            # 세로
+            for k in range(MAX_A):
+                num_i = 0
+                num_j = 0
+                for l in range(MAX_A):
+                    if board[l][k] == i:
+                        num_i += 1
+                    if board[l][k] == j:
+                        num_j += 1
+                if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                    win = True
+            
+            # 오른쪽 대각선
+            num_i = 0
+            num_j = 0
+            for k in range(MAX_A):
+                if board[k][k] == i: # 인덱싱 주목
+                    num_i += 1
+                if board[k][k] == j:
+                    num_j += 1
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+
+            # 왼쪽 대각선
+            num_i = 0
+            num_j = 0
+            for k in range(MAX_A):
+                if board[k][MAX_A - k - 1] == i:
+                    num_i += 1
+                if board[k][MAX_A - k - 1] == j:
+                    num_j += 1
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+
+            if win: # 이렇게 하면 자동으로 중복이 스킵.
+                answer += 1    
+
+    print(answer)   
+
+
 solution()
