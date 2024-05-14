0dbc8d233082d55a605dd31710264ceddeea0478
Somi Park <youyoy689@gmail.com>
2024-05-14 21:44:07 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240514/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,22 +2,21 @@ def solution():
     n = int(input())
     s = input()
     min_length = float("inf")
-    check = [0] * 101
-    for i in range(n):
-        for j in range(i+1,n+1):
-            sub_s = s[i:j]
-            length = j - i
-            if check[length]:
-                continue
-            else:
-                check[length] = 1    
+    check = [0] * (n + 1)
+    for i in range(1, n+1):
+        for j in range(n):
+            sub_s = s[j:j+i]
             count = 0
-            for k in range(n - length + 1):
-                if s[k:k+length] == sub_s:
+            for k in range(n):
+                if s[k:k+i] == sub_s:
                     count += 1
-            if count < 2:
-                min_length = min(min_length, length)
+            if count >= 2:
+                check[i] = 1
+                break
 
+        if not check[i]:
+            min_length = min(i, min_length)    
+        
     print(min_length)    
 
 solution()
