c1d695d91302ccdd285c0a3f984f16ca2d475390
Somi Park <youyoy689@gmail.com>
2024-05-15 21:38:48 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 132ms, Memory: 31MB, Status: Passed - CodeTree

--- 240515/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240515/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -6,15 +6,14 @@ def solution():
 
     max_sum_res = -sys.maxsize
     sum_res = 0
-    flag = False
+    
     for i in range(n):
         if sum_res < 0: 
             sum_res = arr[i]
-            flag = True
-        if not flag:
+        else:
             sum_res += arr[i]
         max_sum_res = max(max_sum_res, sum_res)
-        flag = False
+        
         
     print(max_sum_res)       
 
