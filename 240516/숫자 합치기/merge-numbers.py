b49e7e11e18fb09be7c7abb66d812ae08354dc5e
Somi Park <youyoy689@gmail.com>
2024-05-16 20:34:07 +0900

    Title: 숫자 합치기, Time: 410ms, Memory: 32MB, Status: Passed - CodeTree

--- 240516/숫자 합치기/merge-numbers.py
+++ 240516/숫자 합치기/merge-numbers.py
@@ -1,17 +1,22 @@
 # 이처럼 순간 순간마다 어떤 쌍을 선택해야 하는지에 대한 판단이 확실히 서는 경우에도 그리디 알고리즘을 사용하여 문제를 해결할 수 있습니다.
-from collections import deque
+# 매번 최소를 선택하는 문제엔 우선순위큐가 좋은듯.
+import heapq
 
 
 def solution():
     n = int(input())
-    arr = deque(list(map(int, input().split())))
+    arr = list(map(int, input().split()))
+    pq = []
     answer = 0
-    while len(arr) != 1:
-        arr = deque(sorted(arr))
-        a = arr.popleft()
-        b = arr.popleft()
-        answer += a + b
-        arr.append(a + b)
+
+    for el in arr:
+        heapq.heappush(pq, el)
+
+    while len(pq) != 1:
+        a = heapq.heappop(pq)
+        b = heapq.heappop(pq)
+        answer += (a + b)
+        heapq.heappush(pq, a + b)
        
 
     print(answer)
