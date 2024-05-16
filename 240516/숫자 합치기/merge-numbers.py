da6f07dc3ede9e37ddf90ef3d4a686ed4c5e100c
Somi Park <youyoy689@gmail.com>
2024-05-16 20:29:30 +0900

    Title: 숫자 합치기, Time: 5024ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240516/숫자 합치기/merge-numbers.py
+++ 240516/숫자 합치기/merge-numbers.py
@@ -1,13 +1,15 @@
 # 이처럼 순간 순간마다 어떤 쌍을 선택해야 하는지에 대한 판단이 확실히 서는 경우에도 그리디 알고리즘을 사용하여 문제를 해결할 수 있습니다.
+from collections import deque
+
 
 def solution():
     n = int(input())
-    arr = list(map(int, input().split()))
+    arr = deque(list(map(int, input().split())))
     answer = 0
     while len(arr) != 1:
-        arr.sort(key=lambda x: -x)
-        a = arr.pop()
-        b = arr.pop()
+        arr = deque(sorted(arr))
+        a = arr.popleft()
+        b = arr.popleft()
         answer += a + b
         arr.append(a + b)
        
