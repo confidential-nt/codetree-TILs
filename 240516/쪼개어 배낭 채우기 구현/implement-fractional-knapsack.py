c9621471b59f2e940aced931519fa58fd913eb24
Somi Park <youyoy689@gmail.com>
2024-05-16 20:20:24 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 321ms, Memory: 29MB, Status: Passed - CodeTree

--- 240516/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
+++ 240516/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -1,24 +1,20 @@
+# 해당 문제는 Functional Knapsack -> 쪼개어 담을 수 있음. -> 그리디 사용가능.
+# 쪼개어 담을 수 없는 문제: 0/1 Knapsack -> dp 사용해야
+
 def solution():
     n,m = map(int, input().split())
     arr = [tuple(map(int, input().split())) for _ in range(n)]
     # (무게, 가치)
 
-    temp = [ (v/w, index) for index, (w,v) in enumerate(arr)]
-    temp.sort(reverse=True)
+    arr.sort(key=lambda x: -x[1] / x[0]) # 음수를 이용하여 내림차 순으로 정렬.
+
     answer = 0
-    for i in range(n):
-        if m == 0:
+    for w,v in arr:
+        if m >= w:
+            answer += v
+            m -= w
+        else:
+            answer += v * (m / w)
             break
-
-        if m - arr[temp[i][1]][0] >= 0:
-            m -= arr[temp[i][1]][0]
-            answer += arr[temp[i][1]][1]
-        elif m != 0:
-            answer += arr[temp[i][1]][1] * (m / arr[temp[i][1]][0])
-            m = 0
-    print( "{:.3f}".format(answer))        
-
-
-
-
+    print(f"{answer:.3f}")        
 solution()
