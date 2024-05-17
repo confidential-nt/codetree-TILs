07a1af8e71a139879dd087da5064dd69e7b58ea9
Somi Park <youyoy689@gmail.com>
2024-05-17 17:35:26 +0900

    Title: 최대 숫자 만들기, Time: 591ms, Memory: 30MB, Status: Passed - CodeTree

--- 240517/최대 숫자 만들기/make-biggest-num.py
+++ 240517/최대 숫자 만들기/make-biggest-num.py
@@ -1,3 +1,5 @@
+# 선택된 2개의 값을 모두 고려하여 정렬 기준을 새롭게 설정하는 경우에는 python에서 lambda 함수만으로는 해결이 어렵기 때문에 꼭 custom comparator를 정의하여 해결해야 합니다. python3에서는 custom comparator를 만들고 sort함수의 key 인자로 넘길때 꼭, functools내 cmp_to_key 함수를 import하여 cmp_to_key(compare) 식으로 감싸줘야 합니다.
+
 from functools import cmp_to_key
 
 def solution():
