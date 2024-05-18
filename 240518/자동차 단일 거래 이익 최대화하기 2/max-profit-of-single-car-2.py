1c04975f9cb261b2e83b495690e0b81902652365
Somi Park <youyoy689@gmail.com>
2024-05-18 18:05:17 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 163ms, Memory: 31MB, Status: Passed - CodeTree

--- 240518/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240518/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -6,6 +6,9 @@ def solution():
     min_price = arr[0]
 
     for i in range(n):
+        # 자동차를 파는 시점을 기준으로 그 전에 언제 자동차를 샀어야 하는지를 생각해보는 것.
+        # 자동차를 사야 하는 시점: 파는 시점을 기준으로 앞선 해의 최솟값일 때 구매를 했을 때.
+        # 단, 시간복잡도를 위해 최솟값 갱신도 같이 함 -> 문제의 특성 때문에 가능.
         profit = arr[i] - min_price
 
         if profit > max_profit:
