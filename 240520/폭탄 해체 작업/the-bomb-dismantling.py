662ac94ae1c677bae5097435b6da55180ef1ae48
Somi Park <youyoy689@gmail.com>
2024-05-20 19:29:07 +0900

    Title: 폭탄 해체 작업, Time: 319ms, Memory: 28MB, Status: Passed - CodeTree

--- 240520/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240520/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,17 +1,32 @@
+import heapq
+
 def solution():
     n = int(input())
-    arr = [tuple(map(int, input().split())) for _ in range(n)]
-    arr.sort(key=lambda x:(x[1], x[0]))
+    bombs = [tuple(map(int, input().split())) for _ in range(n)]
+    max_t = max([el[1] for el in bombs])
 
-    answer = 0
-    for i in range(1, n):
-        if arr[i][1] == arr[i - 1][1]:
-            continue
-        answer += arr[i - 1][0]
-        
+    def get_time_limit(bomb_idx):
+        _, time_limit = bombs[bomb_idx]
+        return time_limit
     
-    answer += arr[-1][0]
+    def get_score(bomb_idx):
+        score, _ = bombs[bomb_idx]
+        return score
+    
+    bombs.sort(key=lambda x: (x[1], x[0]))
 
+    pq = []
+    bomb_idx = n - 1
+    answer = 0
+
+    for t in range(max_t, 0, -1):
+        while bomb_idx >= 0 and get_time_limit(bomb_idx) >= t:
+            heapq.heappush(pq, -get_score(bomb_idx))
+            bomb_idx -= 1
+
+        if pq:
+            answer += -heapq.heappop(pq)
+    
     print(answer)
 
 solution()
