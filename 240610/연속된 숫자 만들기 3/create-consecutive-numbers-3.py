e8852f81e5508fc0c5bff42584108bae3b25af75
Somi Park <youyoy689@gmail.com>
2024-06-10 17:19:22 +0900

    Title: 연속된 숫자 만들기 3, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240610/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240610/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,23 +1,23 @@
 def solution():
     arr = list(map(int, input().split()))
-
-    # 1. 이미 정렬 되었을 때 1 2 3
-    # 2. 한 곳만 원하는대로 정렬 되었을 때 1 4 6 -> 1 3 4 -> 1 2 3
-    # 3. 다 정렬 안되었을 때 8 20 21 -> 8 19 20 -> 8 18 19 -> 8 17 18 
+    
+    # 1. 이미 연속한 경우
+    # 2. (각각 거리를 한칸씩 좁혀가면서 이동하는 방식으로 최대 이동횟수 만들기)
+    # 2 - 1. 1이 아니면서, 왼쪽 끝 사람이 가장 먼저 이동하는 경우
+    # 2 - 2. 1이 아니면서, 오른쪽 끝 사람이 가장 먼저 이동하는 경우
+    # 최대로 만들어야하기 때문에 2씩 차이 나나 안나나 분석하는 것은 의미 없음.
 
     if arr[0] + 1 == arr[1] and arr[1] + 1 == arr[2]:
         print(0)
     
-    elif arr[0] + 2 == arr[1] or arr[1] + 2 == arr[2]:
-        print(2)
-
     else:
-        a = arr[2] - arr[1]
-        b = arr[1] - arr[0]
-        if a > b:
-            print(a - b)
-        else:
-            print(b - a)    
-        
+        max_move = 0
+        move = arr[2] - arr[1] - 1
+        max_move = max(max_move, move)
+
+        move = arr[1] - arr[0] - 1
+        max_move = max(max_move, move)
+
+        print(max_move)
 
 solution()
