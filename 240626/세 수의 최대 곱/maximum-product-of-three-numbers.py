5d71b6a67be2bcf9e5a6f2e1a8cb8fa0131b21d8
Somi Park <youyoy689@gmail.com>
2024-06-26 17:39:15 +0900

    Title: 세 수의 최대 곱, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240626/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -5,34 +5,39 @@ def solution():
     arr = list(map(int, input().split()))
 
     # 양의 정수 3개
-    case_1 = 1
+    case_1 = -sys.maxsize
     arr_copy = sorted(arr)
     count = 0
+    result = 1
     for i in range(n-1, -1, -1):
         if count >= 3:
             break
         if arr_copy[i] > 0:
-            case_1 *= arr_copy[i]
+            result *= arr_copy[i]
             count += 1
+    case_1 = result
+
 
     # 양 2, 음 1
-    case_2 = 1
+    case_2 = -sys.maxsize
     arr_copy = sorted(arr, key=lambda x: abs(x))
     count_plus = 0
     count_minus = 0
-
+    result = 1
     for i in range(n):
         if arr_copy[i] == 0:
             continue
         if count_plus < 2 and arr_copy[i] > 0:
-            case_2 *= arr_copy[i]
+            result *= arr_copy[i]
             count_plus += 1
         elif count_minus < 1 and arr_copy[i] < 0:
-            case_2 *= arr_copy[i]
+            result *= arr_copy[i]
             count_minus += 1
+    case_2 = result
 
     # 양1, 음2
-    case_3 = 1
+    case_3 = -sys.maxsize
+    result = 1
     arr_copy = sorted(arr, key=lambda x: abs(x))
     count_plus = 0
     count_minus = 0
@@ -40,27 +45,31 @@ def solution():
         if arr_copy[i] == 0:
             continue
         if count_plus < 1 and arr_copy[i] > 0:
-            case_3 *= arr_copy[i]
+            result *= arr_copy[i]
             count_plus += 1
         elif count_minus < 2 and arr_copy[i] < 0:
-            case_3 *= arr_copy[i]
+            result *= arr_copy[i]
             count_minus += 1
+    case_3 = result
 
     # 음 3
-    case_4 = 1
+    case_4 = -sys.maxsize
+    result = 1
     arr_copy = sorted(arr, key=lambda x: abs(x))
     count = 0
     for i in range(n):
         if count >= 3:
             break
         if arr_copy[i] < 0:
-            case_4 *= arr_copy[i]
+            result *= arr_copy[i]
             count += 1
+    case_4 = result
+
     # 영 포함
     case_5 = -sys.maxsize
     for i in range(n):
         if arr[i] == 0:
             case_5 = 0
-            
+
     print(max(case_1, case_2, case_3, case_4, case_5))
 solution()
