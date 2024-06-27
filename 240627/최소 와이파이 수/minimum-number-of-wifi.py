678282d386c7ee33f13b11fffb3e12f5b7e2b078
Somi Park <youyoy689@gmail.com>
2024-06-27 22:46:08 +0900

    Title: 최소 와이파이 수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240627/최소 와이파이 수/minimum-number-of-wifi.py
@@ -25,3 +25,25 @@ def solution():
 
 
 solution()
+
+
+def solution2():
+    # 변수 선언 및 입력:
+    n, m = tuple(map(int, input().split()))
+    arr = list(map(int, input().split()))
+
+    # 사람이 살고 있는 곳이 나오면
+    # 와이파이를 해당 위치로부터 오른쪽으로 m만큼 떨어진 곳에 놓은 뒤,
+    # 2m만큼 떨어진 곳에서 시작하여 다시 탐색을 진행합니다.
+    # 배열을 복사할 필요 없이 인덱스 만으로도 해결가능....
+    # 문제에서 가장 중요한 것은 1이 어디서 시작하냐는 것이고 그 인덱스만을 기반으로 하면
+    # 답이 나옴.
+    cnt, i = 0, 0
+    while i < n:
+        if arr[i] == 1:
+            cnt += 1
+            i += 2 * m + 1
+        else:
+            i += 1
+
+    print(cnt)
