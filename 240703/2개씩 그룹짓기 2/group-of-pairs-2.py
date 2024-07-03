eb35da8d349cb8b84b59d1d8816acf641df269c4
Somi Park <youyoy689@gmail.com>
2024-07-03 19:02:27 +0900

    Title: 2개씩 그룹짓기 2, Time: 203ms, Memory: 40MB, Status: Passed - CodeTree

--- 240703/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240703/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,29 +1,17 @@
-# n개의 그룹으로 만들어
-# 각 그룹내의 2개의 수의 차이 중
-# 최솟값이 최대가 되도록 하는
+def solution():
+    # 2 5 7 9 10 15
+    # 예제 관찰 -> 2개의 수 차이 중 최솟값이 최대가 되도록... -> 정렬 후 n만큼 떨어진 사람끼리 묶는 것이 최선
+    # 그룹으로 묶인 두 사람이 너무 가까이 있으면 차이가 너무 작아져 최대를 만들기 어려워지고
+    # 그렇다고 두 사람이 너무 멀리있다면 남은 사람들끼리 차이를 크게 만들기가 어려움
+    
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    answer = float("inf")
+    for i in range(n):
+        # 이미 최대가 되는 조합
+        diff = arr[i + n] - arr[i]
+        answer = min(answer, diff)
+    print(answer)
 
-import sys
-
-si = sys.stdin.readline
-
-# 1 <= n <= 100,000 -> n^2 만 해도 100억 이중 포문 풀이로 불가능
-n = int(si())
-
-arr = [0] + list(map(int, si().split()))
-
-# 2n 개의 원소를 n개의 그룹으로 쪼개기 
-# 두 원소의 차이의 최솟값을 최대로 하기 위해선...
-# 차이의 최솟값을 최대..
-
-arr.sort()
-
-# print(arr)
-# 2 5 7 9 10 15
-
-# (1, n + 1) (2, n + 2) ... 이런식으로 짝지으면 되나?
-
-min_diff = sys.maxsize 
-for i in range(1, n + 1):
-    min_diff = min(min_diff, arr[n + i] - arr[i])
-
-print(min_diff)
+solution()
