c340148494c41fddec1cd7a2e35d299864dfe245
Somi Park <youyoy689@gmail.com>
2024-07-05 18:21:57 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240705/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240705/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,21 +6,18 @@ def solution():
     n = len(string)
     for i in range(n):
         char = string[i]
-        if not len(stack):
-            stack.append(char)
-            continue
 
         top = stack[-1]
         
-        if top == "(":
-            if char == ")":
+        if char == "(": # (인 경우엔 무조건 넣음.
+            stack.push(char)
+        else: # ) 인데 스택이 비어있다면, 올바른 괄호 될 가망 없음.
+            if not stack:
+                print("No")
+                return
+            else: # ) 가 첫번째 원소로 들어올리가 없으므로 비어있지 않다면 그건 무조건 ).
                 stack.pop()
-            else:
-                stack.append(char)
-        else:
-            stack.append(char)
 
-    
     if len(stack):
         print("No")
     else:
