54593ea6bcdd5c14b6f0df4e6c3e15115af1be6e
Somi Park <youyoy689@gmail.com>
2024-07-12 18:25:56 +0900

    Title: 대응되는 수와 문자, Time: 606ms, Memory: 41MB, Status: Passed - CodeTree

--- 240712/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240712/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -15,4 +15,36 @@ def solution():
             print(d1[int(q)])
 
 
+def solution2():
+    # 이렇게 풀 수도 있음.
+    # 변수 선언 및 입력:
+    n, m = tuple(map(int, input().split()))
+
+    # 다음과 같이 입력받으면 첫 번째 값이 0번 index에 저장된다는 점을 기억합시다.
+    words = [
+        input()
+        for _ in range(n)
+    ]
+
+    string_to_num = dict()
+
+    # 각 숫자의 대응되는 문자를 array에,
+    # 각 문자의 대응되는 숫자를 hashmap에 기록해줍니다.
+    for i in range(n):
+        string_to_num[words[i]] = i + 1
+
+    for _ in range(m):
+        word = input()
+
+        # 입력된 값이 숫자일 때에는 array에 저장한 문자를 출력합니다.
+        # ord: 유니코드 얻기
+        if ord(word[0]) >= ord('0') and ord(word[0]) <= ord('9'):
+            num = int(word)
+            # n번에 대응되는 문자열이 n-1번 index에 들어있기 때문에 1을 빼줍니다.
+            print(words[num - 1])
+        
+        # 입력된 값이 문자일 때에는 hashmap에 기록된 대응되는 숫자를 출력합니다.
+        else:
+            print(string_to_num[word])
+
 solution()
