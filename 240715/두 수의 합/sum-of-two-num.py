8f2ebd376fa432dfbf9612a684a02f775cf5bbbb
Somi Park <youyoy689@gmail.com>
2024-07-15 15:10:20 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,30 @@
+def solution():
+    n,k = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    d = dict()
+    for el in arr:
+        if el not in d:
+            d[el] = 0
+        d[el] += 1
+
+    count = 0
+    for el in arr:
+        target = k - el
+        if target in d:
+            if d[el] - d[target] == 0:
+                count += d[el]
+                d.pop(el)
+                d.pop(target)
+            elif d[el] - d[target] < 0:
+                count += d[el]
+                d[target] = d[target] - d[el]
+                d.pop(el)
+            elif dl[el] - d[target] > 0:
+                count += d[target]
+                d[el] = d[el] - d[target]
+                d.pop(target)
+    print(count)
+
+
+solution()
