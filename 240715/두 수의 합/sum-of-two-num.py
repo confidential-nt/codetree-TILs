2d8dca05dd6a2223972b845b4a9a8fe8d202bf15
Somi Park <youyoy689@gmail.com>
2024-07-15 15:13:04 +0900

    Title: 두 수의 합, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/두 수의 합/sum-of-two-num.py
+++ 240715/두 수의 합/sum-of-two-num.py
@@ -12,18 +12,25 @@ def solution():
     for el in arr:
         target = k - el
         if target in d:
-            if d[el] - d[target] == 0:
-                count += d[el]
-                d.pop(el)
+            d[target] -= 1
+            d[el] -= 1
+            count += 1
+            if d[target] == 0:
                 d.pop(target)
-            elif d[el] - d[target] < 0:
-                count += d[el]
-                d[target] = d[target] - d[el]
+            if d[el] == 0:
                 d.pop(el)
-            elif dl[el] - d[target] > 0:
-                count += d[target]
-                d[el] = d[el] - d[target]
-                d.pop(target)
+            # if d[el] - d[target] == 0:
+            #     count += d[el]
+            #     d.pop(el)
+            #     d.pop(target)
+            # elif d[el] - d[target] < 0:
+            #     count += d[el]
+            #     d[target] = d[target] - d[el]
+            #     d.pop(el)
+            # elif dl[el] - d[target] > 0:
+            #     count += d[target]
+            #     d[el] = d[el] - d[target]
+            #     d.pop(target)
     print(count)
 
 
