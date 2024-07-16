b43150882ccbe2392ec3e78df7bbf969c077b059
Somi Park <youyoy689@gmail.com>
2024-07-16 19:53:02 +0900

    Title: 세 수의 합, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/세 수의 합/sum-of-three-num.py
+++ 240716/세 수의 합/sum-of-three-num.py
@@ -57,15 +57,15 @@ def solution():
         # 이미 순회한 적이 있는 숫자는 빼 버림으로서
         # 같은 조합이 여러번 세어지는 걸 방지합니다.
         count[arr[i]] -= 1
-
-        for j in range(i):
+        
+        for j in range(i): # 순회 범위에 유의. 이렇게 하면 중복 없이 모든 구간을 다 돌 수 있음.
             # 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
             # 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
             diff = k - arr[i] - arr[j]
 
             if diff in count:
                 ans += count[diff]
-
+    
     print(ans)
 
 solution()
