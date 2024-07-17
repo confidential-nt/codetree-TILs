58d95923e9007e34a1334ceff51dfa0f89e263bb
Somi Park <youyoy689@gmail.com>
2024-07-17 17:55:43 +0900

    Title: 자주 등장한 top k 숫자, Time: 426ms, Memory: 36MB, Status: Passed - CodeTree

--- 240717/자주 등장한 top k 숫자/top-k-frequent-elements.py
+++ 240717/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -11,4 +11,35 @@ def solution():
         print(el[0], end=" ")
 
 
+def solution2():
+    # 변수 선언 및 입력:
+    n, k = tuple(map(int, input().split()))
+    arr = list(map(int, input().split()))
+
+    count = dict()
+
+    # 각 숫자가 몇 번씩 나왔는지를
+    # hashmap에 기록해줍니다.
+    for elem in arr:
+        if elem in count:
+            count[elem] += 1
+        else:
+            count[elem] = 1
+
+    # hashmap을 순회하며
+    # 중복되지 않게 새 배열을 만들어 줍니다.
+    new_arr = [
+        [value, key]
+        for key, value in count.items()
+    ]
+
+    # 문제에서 요구한 정렬 기준에 맞추어 정렬합니다.
+    new_arr = sorted(new_arr)
+
+    # 출력:
+    leng = len(new_arr)
+    for i in range(leng - 1, leng - k - 1, -1):
+        print(new_arr[i][1], end=" ")
+
+
 solution()
